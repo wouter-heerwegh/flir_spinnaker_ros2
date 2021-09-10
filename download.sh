655e7748164feda16595830e76738ffeebca59e4
Wouter Heerwegh <wouter.heerwegh@gmail.com>
2021-09-10 13:28:35 +0200

    Added download.sh to download dependencies

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ..
+git clone git@github.com:berndpfrommer/camera_control_msgs_ros2.git
+git clone git@github.com:berndpfrommer/image_meta_msgs_ros2.git
+git clone git@github.com:berndpfrommer/flir_spinnaker_common.git
